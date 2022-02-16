7bb2f64d5cac45fc91672afdcd6dca21363e3668
hossain101199 <hossainbhai1011@gmail.com>
2022-02-17 02:39:26 +0600

    js clculator has been done

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
+// Input
+let IncomeIncome = document.getElementById("IncomeInput")
+let FoodExpenses = document.getElementById("FoodInput");
+let RentExpenses = document.getElementById("RentInput");
+let ClothesExpenses = document.getElementById("ClothesInput");
+let SavePercentage = document.getElementById("SaveInput");
+// Output
+let totalExpensesOutput = document.getElementById("TotalExpensesID");
+let BalanceOutput = document.getElementById("BalanceExistID");
+let SavingOutput = document.getElementById("SavingAmountID");
+let RemainingOutput = document.getElementById("RemainingID");
 
-let TotalExpenses = 0;
 function clculator() {
-    let IncomeIncome = document.getElementById("IncomeInput")
-    let FoodExpenses = document.getElementById("FoodInput");
-    let RentExpenses = document.getElementById("RentInput");
-    let ClothesExpenses = document.getElementById("ClothesInput");
-    let totalExpensesOutput = document.getElementById("TotalExpensesID")
-    let BalanceOutput = document.getElementById("BalanceExistID")
     totalExpensesOutput.innerText = Number(FoodExpenses.value) + Number(RentExpenses.value) + Number(ClothesExpenses.value) + Number(totalExpensesOutput.innerText);
     BalanceOutput.innerText = Number(IncomeIncome.value) - Number(totalExpensesOutput.innerText);
     IncomeIncome.value = "";
     FoodExpenses.value = "";
     RentExpenses.value = "";
     ClothesExpenses.value = "";
+
+}
+
+function Savingclculator() {
+    // 1st
+    totalExpensesOutput.innerText = Number(FoodExpenses.value) + Number(RentExpenses.value) + Number(ClothesExpenses.value) + Number(totalExpensesOutput.innerText);
+    BalanceOutput.innerText = Number(IncomeIncome.value) - Number(totalExpensesOutput.innerText);
+    // 2nd
+    SavingOutput.innerText = Number(IncomeIncome.value) * (Number(SavePercentage.value) / 100)
+    RemainingOutput.innerText = Number(BalanceOutput.innerText) - Number(SavingOutput.innerText);
+    // 1st
+    IncomeIncome.value = "";
+    FoodExpenses.value = "";
+    RentExpenses.value = "";
+    ClothesExpenses.value = "";
+
+    // 2nd
+    IncomeIncome.value = "";
+    SavePercentage.value = "";
 
 }
